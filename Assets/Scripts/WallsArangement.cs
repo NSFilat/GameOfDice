41f23e5a96868dfaf82cd27ce775354c350805ad
Alexandrovich <dmitry_jeleznov@icloud.com>
2021-07-28 16:30:41 +0300

    28.07.2021

--- Assets/Scripts/WallsArangement.cs
+++ Assets/Scripts/WallsArangement.cs
@@ -4,12 +4,43 @@ using UnityEngine;
 
 public class WallsArangement : MonoBehaviour
 {
+    public Transform LeftWall;
+    public Transform RightWall;
+    public Transform UpperWall;
+    public Transform LowerWall;
     // Start is called before the first frame update
     void Awake()
     {
         //gameObject.transform.position = new Vector3(Screen.width, Screen)
 
         print($"Screen width = {Screen.width}  Screen heigth = {Screen.height}");
+        //Camera.main.he = 0.5625f;
+        print(Camera.main.aspect);
+
+        /*
+        float left_x = -0.3f * ((float)(Screen.height) / Screen.width) / (16.0f / 9.0f);
+        float left_z = -4.2f * ((float)(Screen.height) / Screen.width) / (16.0f / 9.0f);
+
+        LeftWall.position = new Vector3(left_x, 0, left_z);
+
+        print(LeftWall.position);
+
+        float right_x = -0.3f * ((float)(Screen.height) / Screen.width) / (16.0f / 9.0f);
+        float right_z = 4.2f * ((float)(Screen.height) / Screen.width) / (16.0f / 9.0f);
+
+        RightWall.position = new Vector3(right_x, 0, right_z);
+
+        float upper_x = -7.13f * ((float)(Screen.height) / Screen.width) / (16.0f / 9.0f);
+        float upper_z = 0.08f * ((float)(Screen.height) / Screen.width) / (16.0f / 9.0f);
+
+        UpperWall.position = new Vector3(upper_x, 0, upper_z);
+
+        float lower_x = 7.13f * ((float)(Screen.height) / Screen.width) / (16.0f / 9.0f);
+        float lower_z = -0.08f * ((float)(Screen.height) / Screen.width) / (16.0f / 9.0f);
+
+        LowerWall.position = new Vector3(lower_x, 0, lower_z);
+        */
+
     }
 
     // Update is called once per frame
