cd828968135d76f4f3f2ad77db6485a9c6070a30
NSFilat <nsfilat@ynadex.ru>
2021-07-29 13:56:44 +0300

    Added constant framerate

--- Assets/Scripts/FrameRate.cs
+++ Assets/Scripts/FrameRate.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Position : MonoBehaviour
+public class FrameRate : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        print(transform.position.y);
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
