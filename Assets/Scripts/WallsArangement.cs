aeb7d32e1a02032a52352e5acbb4beacf0c7e252
NSFilat <nsfilat@yandex.ru>
2021-09-17 12:17:03 +0300

    Refactoring

--- Assets/Scripts/WallsArangement.cs
+++ Assets/Scripts/WallsArangement.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class WallsArangement : MonoBehaviour
 {
-    private const float Size_x = 13.24f;
-    private const float Square = 49.30245f;
+    private const float _Size_x = 13.24f;
+    private const float _Square = 49.30245f;
 
-    [SerializeField] private readonly float horizontalOffset = 0.5f;
-    [SerializeField] private readonly float verticalOffset = 2f;
-    [SerializeField] private readonly float floorOffset = 10f;
+    [SerializeField] private readonly float _horizontalOffset = 0.5f;
+    [SerializeField] private readonly float _verticalOffset = 2f;
+    [SerializeField] private readonly float _floorOffset = 10f;
 
     [SerializeField] private Transform _upperWall;
     [SerializeField] private Transform _lowerWall;
@@ -18,43 +18,45 @@ public class WallsArangement : MonoBehaviour
     [SerializeField] private Transform _floor;
     [SerializeField] private Transform _dice;
 
+    private int _width = Screen.width;
+    private int _height = Screen.height;
 
-    void Start()
+    private void Start()
     {
-        int width = Screen.width;
-        int height = Screen.height;
+        _width /= FindGCD(_width, _height);
+        _height /= FindGCD(_width, _height);
 
-        //Debug.Log($"{width} + {height}");
-
-        int GCD = FindGCD(width, height);
-
-        width /= GCD;
-        height /= GCD;
-
-        DefineSize(width, height, out float position_z);
-
-        _leftWall.position = new Vector3(_leftWall.position.x, 0, -position_z - horizontalOffset);
-        _rightWall.position = new Vector3(_rightWall.position.x, 0, position_z + horizontalOffset);
-
-        _upperWall.localScale = new Vector3(_upperWall.localScale.x, _upperWall.localScale.y, position_z * 2 + verticalOffset);
-        _lowerWall.localScale = new Vector3(_lowerWall.localScale.x, _lowerWall.localScale.y, position_z * 2 + verticalOffset);
-
-        _floor.localScale = new Vector3((Mathf.Abs(_upperWall.position.x) * 2 + 1) / floorOffset, 1, (Mathf.Abs(_rightWall.position.z) * 2 + 1) / floorOffset);
-        
-        float scale = 70 * position_z * Size_x / Square;
-        _dice.localScale = new Vector3(scale, scale, scale);
-
-        //Debug.Log(_dice.localScale);
+        ChangeWallsPosition(DefineSize(_width, _height));
+        ChangeDiceScale(DefineSize(_width, _height));
     }
 
-    private void DefineSize(int width, int height, out float position_z)
+
+    private float DefineSize(int width, int height)
     {
-        float coefficient = Size_x / height;
-        position_z = width * coefficient / 2;
+        float coefficient = _Size_x / height;
+        return  width * coefficient / 2;
     }
 
     private int FindGCD(int a, int b)
     {
         return b == 0 ? a : FindGCD(b, a % b);
+    }
+
+    private void ChangeWallsPosition(float position_z)
+    {
+        _leftWall.position = new Vector3(_leftWall.position.x, 0, -position_z - _horizontalOffset);
+        _rightWall.position = new Vector3(_rightWall.position.x, 0, position_z + _horizontalOffset);
+
+        _upperWall.localScale = new Vector3(_upperWall.localScale.x, _upperWall.localScale.y, position_z * 2 + _verticalOffset);
+        _lowerWall.localScale = new Vector3(_lowerWall.localScale.x, _lowerWall.localScale.y, position_z * 2 + _verticalOffset);
+
+        _floor.localScale = new Vector3((Mathf.Abs(_upperWall.position.x) * 2 + 1) / _floorOffset, 1, (Mathf.Abs(_rightWall.position.z) * 2 + 1) / _floorOffset);
+
+    }
+
+    private void ChangeDiceScale(float position_z)
+    {
+        float scale = 70 * position_z * _Size_x / _Square;
+        _dice.localScale = new Vector3(scale, scale, scale);
     }
 }
