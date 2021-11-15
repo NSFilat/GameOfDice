acc2055246fa3c17ca712ed1e859ae37a9a5e04d
NSFilat <nsfilat@ynadex.ru>
2021-11-15 16:54:20 +0300

    play

---
+++ Assets/Ads.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Ads : MonoBehaviour, IUnityAdsInitializationListener  
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
