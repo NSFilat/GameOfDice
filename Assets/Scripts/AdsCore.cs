1904ff05d303456560db5233b6c4bacc79c0ae30
NSFilat <nsfilat@ynadex.ru>
2021-11-15 16:58:58 +0300

    Прохожего на себя похожего

---
+++ Assets/Scripts/AdsCore.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdsCore : MonoBehaviour
+{
+    [SerializeField] private bool _testMode = true;
+
+    private string _gameId = "4453209";
+
+
+}
