fdf6aecf3c94bfcf899a0f54f83d91b5baea3e03
NSFilat <nsfilat@ynadex.ru>
2021-11-20 18:47:22 +0300

    The banner was fixed

--- Assets/Scripts/MobAdsBanner.cs
+++ Assets/Scripts/MobAdsBanner.cs
@@ -8,19 +8,10 @@ public class MobAdsBanner : MonoBehaviour
     private BannerView bannerView;
     private const string bannerUnitId = "ca-app-pub-3940256099942544/6300978111";
 
-    private AdSize myAdSize = new AdSize(1920, 1080);
-
     private void OnEnable()
     {
         bannerView = new BannerView(bannerUnitId, AdSize.Banner, AdPosition.Bottom);
         AdRequest adRequest = new AdRequest.Builder().Build();
         bannerView.LoadAd(adRequest);
-        StartCoroutine(ShowBanner());
-    }
-
-    IEnumerator ShowBanner()
-    {
-        yield return new WaitForSecondsRealtime(2f);
-        bannerView.Show();
-    }    
+    }   
 }
